ab40b4ac6d9e81c9b0b8684304bffcc8cf420a54
Levi Villarreal <villarreallevi@gmail.com>
2025-10-05 04:29:27 -0400

    format

--- src/components/Draggable.svelte
+++ src/components/Draggable.svelte
@@ -1,36 +1,40 @@
 <script lang="ts">
-	export let left = 100;
-	export let top = 100;
+  export let left = 100;
+  export let top = 100;
 
-	let moving = false;
+  let moving = false;
 
-	function onMouseDown() {
-		moving = true;
-	}
+  function onMouseDown() {
+    moving = true;
+  }
 
-	function onMouseMove(e: MouseEvent) {
-		if (moving) {
-			left += e.movementX;
-			top += e.movementY;
-		}
-	}
+  function onMouseMove(e: MouseEvent) {
+    if (moving) {
+      left += e.movementX;
+      top += e.movementY;
+    }
+  }
 
-	function onMouseUp() {
-		moving = false;
-	}
+  function onMouseUp() {
+    moving = false;
+  }
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<section on:mousedown={onMouseDown} style="left: {left}px; top: {top}px;" class="draggable">
-	<slot></slot>
+<section
+  on:mousedown={onMouseDown}
+  style="left: {left}px; top: {top}px;"
+  class="draggable"
+>
+  <slot></slot>
 </section>
 
 <svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
 
 <style>
-	.draggable {
-		user-select: none;
-		cursor: move;
-		position: absolute;
-	}
+  .draggable {
+    user-select: none;
+    cursor: move;
+    position: absolute;
+  }
 </style>
